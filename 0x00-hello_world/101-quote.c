d69d2f25de4facb0226013f58063d41ded5cd24f
Zachariarian <zachariarian@gmail.com>
2022-04-07 03:40:23 -0700

    task last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s=S9);
+return (1);
+}

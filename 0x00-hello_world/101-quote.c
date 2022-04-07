ac8d2b4fdbd4f7a7e5df142409cfe3afb6dc8fbb
Zachariarian <zachariarian@gmail.com>
2022-04-07 03:54:29 -0700

    task last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", S9);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 9);
 return (1);
 }

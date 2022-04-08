6fd03425adce76d130a1e842bf17579256a091f5
Zachariarian <zachariarian@gmail.com>
2022-04-08 04:59:04 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+
+if (number == 57)
+{
+break;
+}
+
+putchar('.');
+putchar(' ');
+}
+return (0);
+}

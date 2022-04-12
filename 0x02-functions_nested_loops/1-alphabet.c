bc70a86140f5af6455b2d7f18694f21dd435742a
Zachariarian <zachariarian@gmail.com>
2022-04-12 05:31:58 -0700

    4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,30 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+include "main.h"
+
 
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
- *Return: Always 0
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
+	
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 
-if ((n % 10) > 5)
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+_putchar(alphabet);
 }
-else if ((n % 10) < 6 && (n % 10) != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-}
-else
-{
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
-}
-return (0);
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 
 
 /**
+ * print_alphabet_x10 - print alphabet
  *
- * main - Prints the alphabet in lowercase.
  * Return: Always 0.
  */
 
-int main(void)
+void print_alphabet_x10(void)
 
 {
-char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++)
+int alphabet;
+int count;
 
-putchar(letter);
-putchar('\n');
-
-return (0);
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+count++;
+_putchar('\n');
+}
 }

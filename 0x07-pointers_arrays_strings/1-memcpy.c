d4aed417a6f17a610c004b68b29ef1b039a63b28
Zacharia Ria Ndungu <101341486+Zachariarian@users.noreply.github.com>
2022-04-27 09:54:29 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
- * *_memcpy - copies memory area
- * @dest: memory area
- * @src: source
- * @n: length of src
- *
- * Return: pointer to dest.
+ *_memcpy - copies memory area
+ *@dest:destination
+ *@src:source
+ *@n:no. of bytes
+ *Return:pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
